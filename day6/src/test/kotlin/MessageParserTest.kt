9fd35df84c57d2cea2480f9177ea202ee118cd68
Stuart Kent <skent.dev@gmail.com>
2016-12-06 23:05:31 -0500

    Clean up day 6 tests

--- day6/src/test/kotlin/MessageParserTest.kt
+++ day6/src/test/kotlin/MessageParserTest.kt
@@ -3,29 +3,34 @@ import io.kotlintest.specs.BehaviorSpec
 class MessageParserTest : BehaviorSpec() {
 
   init {
-    Given("") {
-      When("") {
-        Then("") {
-          val receivedMessages = listOf(
-              "eedadn",
-              "drvtee",
-              "eandsr",
-              "rasrtv",
-              "nssdts",
-              "raavrd",
-              "atevrs",
-              "tsrnev",
-              "sdttsa",
-              "ntnada",
-              "svetve",
-              "tesnvt",
-              "vntsnd",
-              "vrdear",
-              "dvrsen",
-              "enarar"
-          )
+    Given("the relatively short sequence of received messages provided in the problem statement") {
+      val receivedMessages = listOf(
+          "eedadn",
+          "drvtee",
+          "eandsr",
+          "rasrtv",
+          "nssdts",
+          "raavrd",
+          "atevrs",
+          "tsrnev",
+          "sdttsa",
+          "ntnada",
+          "svetve",
+          "tesnvt",
+          "vntsnd",
+          "vrdear",
+          "dvrsen",
+          "enarar"
+      )
 
+      When("I calculate the real message by determining the highest frequency character at each index") {
+        Then("the result is \"easter\"") {
           MessageParser(receivedMessages).parseUsingHighestFrequencyCharacters() shouldBe "easter"
+        }
+      }
+
+      When("I calculate the real message by determining the lowest frequency character at each index") {
+        Then("the result is \"advent\"") {
           MessageParser(receivedMessages).parseUsingLowestFrequencyCharacters()  shouldBe "advent"
         }
       }
