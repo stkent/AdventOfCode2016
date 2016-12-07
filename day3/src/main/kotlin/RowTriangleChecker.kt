4f3de4cb45a8871824ab3cff990d34193c5e739d
Stuart Kent <skent.dev@gmail.com>
2016-12-06 22:49:26 -0500

    Replace readLines.forEach with forEachLine
    forEachLine handles large files better!

--- day3/src/main/kotlin/RowTriangleChecker.kt
+++ day3/src/main/kotlin/RowTriangleChecker.kt
@@ -1,19 +1,19 @@
 import java.io.File
 
 class RowTriangleChecker : BaseTriangleChecker() {
-  
+
   override fun countNumberOfValidTriangles(inputFile: File) : Int {
     var result = 0
-    
-    inputFile.readLines().forEach {
+
+    inputFile.forEachLine {
       val triangleSides = it.trim().split(spaceSplitRegex).map(String::toInt)
 
       if (isValidTriangle(triangleSides)) {
         result++
       }
     }
-    
+
     return result
   }
-  
+
 }
