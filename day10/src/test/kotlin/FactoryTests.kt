176f9d062dc17496114d8e45fd2fef60feca56ad
Stuart Kent <skent.dev@gmail.com>
2016-12-10 12:45:00 -0500

    Update day 10 tests; fix file name

--- day10/src/test/kotlin/FactoryTests.kt
+++ day10/src/test/kotlin/FactoryTests.kt
@@ -3,13 +3,27 @@ import io.kotlintest.specs.BehaviorSpec
 class FactoryTests : BehaviorSpec() {
 
   init {
-//    value 5 goes to botNumber 2
-//    botNumber 2 gives low to botNumber 1 and high to botNumber 0
-//    value 3 goes to botNumber 1
-//    botNumber 1 gives low to output 1 and high to botNumber 0
-//    botNumber 0 gives low to output 2 and high to output 0
-//    value 2 goes to botNumber 2
+    Given("a factory") {
+      When("I process a set of instructions") {
+        Then("all chips should end up in the correct output bins") {
+          val factory = Factory()
+          factory.processInstructions(listOf(
+              "value 5 goes to bot 2",
+              "bot 2 gives low to bot 1 and high to bot 0",
+              "value 3 goes to bot 1",
+              "bot 1 gives low to output 1 and high to bot 0",
+              "bot 0 gives low to output 2 and high to output 0",
+              "value 2 goes to bot 2"
+          ))
 
+          factory.outputs shouldBe mapOf(
+              Pair(0, 5),
+              Pair(1, 2),
+              Pair(2, 3)
+          )
+        }
+      }
+    }
   }
 
 }
