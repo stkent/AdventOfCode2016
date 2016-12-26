6bfd4283c2cf0653924318f966d5d8406d0892b4
Stuart Kent <skent.dev@gmail.com>
2016-12-26 16:03:46 -0600

    Clean up day 14 tests

--- day14/src/test/kotlin/extensions/StringTests.kt
+++ day14/src/test/kotlin/extensions/StringTests.kt
@@ -6,101 +6,153 @@ class StringTests : BehaviorSpec() {
 
   init {
     Given("a string in which all characters match") {
+      val string = "aaaaa"
+
       When("I query whether all characters match") {
+        val allCharactersMatch = string.allCharactersMatch()
+
         Then("I am correctly told that they do") {
-          "aaaaa".allCharactersMatch() shouldBe true
+          allCharactersMatch shouldBe true
         }
       }
 
-      When("I query whether all characters match that character") {
+      When("I query whether all characters match the repeated character") {
+        val allCharactersMatchRepeatedCharacter = string.allCharactersMatch('a')
+
         Then("I am correctly told that they do") {
-          "aaaaa".allCharactersMatch('a') shouldBe true
+          allCharactersMatchRepeatedCharacter shouldBe true
         }
       }
 
       When("I query whether all characters match some other character") {
+        val allCharactersMatchSomeOtherCharacter = string.allCharactersMatch('b')
+
         Then("I am correctly told that they do not") {
-          "aaaaa".allCharactersMatch('b') shouldBe false
+          allCharactersMatchSomeOtherCharacter shouldBe false
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          "aaabbcc".firstRepeatedChar(3) shouldBe 'a'
+    Given("a string that starts with a single character repeated 3 times, and no other characters are repeated") {
+      val string = "aaabcde"
+
+      When("I ask for the first character that is repeated at least 3 times") {
+        val firstCharRepeatedAtLeast3Times = string.firstRepeatedChar(minRepetitions = 3)
+
+        Then("the correct character should be identified") {
+          firstCharRepeatedAtLeast3Times shouldBe 'a'
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          "aabbbcc".firstRepeatedChar(3) shouldBe 'b'
+    Given("a string with a single character repeated 3 times in the middle, and no other characters are repeated") {
+      val string = "abcccde"
+
+      When("I ask for the first character that is repeated at least 3 times") {
+        val firstCharRepeatedAtLeast3Times = string.firstRepeatedChar(minRepetitions = 3)
+
+        Then("the correct character should be identified") {
+          firstCharRepeatedAtLeast3Times shouldBe 'c'
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          "aabbccc".firstRepeatedChar(3) shouldBe 'c'
+    Given("a string that ends with a single character repeated 3 times, and no other characters are repeated") {
+      val string = "abcdeee"
+
+      When("I ask for the first character that is repeated at least 3 times") {
+        val firstCharRepeatedAtLeast3Times = string.firstRepeatedChar(minRepetitions = 3)
+
+        Then("the correct character should be identified") {
+          firstCharRepeatedAtLeast3Times shouldBe 'e'
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          "aaabbbccc".containsRepeatedChar('a', 3) shouldBe true
+    Given("a string that starts with a single character repeated 3 times") {
+      val string = "aaabbbccc"
+
+      When("I ask if the string contains that character repeated at least 3 times") {
+        val containsSpecifiedCharRepeatedAtLeast3Times = string.containsRepeatedChar('a', minRepetitions = 3)
+
+        Then("the result should be true") {
+          containsSpecifiedCharRepeatedAtLeast3Times shouldBe true
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          "aaabbbccc".containsRepeatedChar('b', 3) shouldBe true
+    Given("a string that has a single character repeated 3 times in the middle") {
+      val string = "aaabbbccc"
+
+      When("I ask if the string contains that character repeated at least 3 times") {
+        val containsSpecifiedCharRepeatedAtLeast3Times = string.containsRepeatedChar('b', minRepetitions = 3)
+
+        Then("the result should be true") {
+          containsSpecifiedCharRepeatedAtLeast3Times shouldBe true
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          "aaabbbccc".containsRepeatedChar('c', 3) shouldBe true
+    Given("a string that ends with a single character repeated 3 times") {
+      val string = "aaabbbccc"
+
+      When("I ask if the string contains that character repeated at least 3 times") {
+        val containsSpecifiedCharRepeatedAtLeast3Times = string.containsRepeatedChar('c', minRepetitions = 3)
+
+        Then("the result should be true") {
+          containsSpecifiedCharRepeatedAtLeast3Times shouldBe true
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          "aaabbbccc".containsRepeatedChar('a', 2) shouldBe true
+    Given("a string that contains at least one substring of length at least 3 consisting of a single repeated character") {
+      val string = "aaabcde"
+
+      When("I asked if the string contains that character repeated at least 2 times") {
+        val containsSpecifiedCharRepeatedAtLeast2Times = string.containsRepeatedChar('a', minRepetitions = 2)
+
+        Then("the result should be true") {
+          containsSpecifiedCharRepeatedAtLeast2Times shouldBe true
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          "aaabbbccc".containsRepeatedChar('a', 4) shouldBe false
+    Given("a string that contains no substrings of length at least 4 consisting of a single repeated character") {
+      val string = "aaabcdeaaa"
+
+      When("I asked if any of the string characters appear in four consecutive locations") {
+        val containsSequenceOf4ConsecutiveChars = string
+            .toCharArray()
+            .distinct()
+            .map { string.containsRepeatedChar(it, minRepetitions = 4) }
+            .fold(initial = false) { b1, b2 -> b1 || b2 }
+
+        Then("the result should be false") {
+          containsSequenceOf4ConsecutiveChars shouldBe false
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          "347dac6ee8eeea4652c7476d0f97bee5".firstRepeatedChar(3) shouldBe 'e'
+    Given("the first sample string from the puzzle statement") {
+      val string = "347dac6ee8eeea4652c7476d0f97bee5"
+
+      When("I ask for the first character repeated at least 3 times") {
+        val firstCharRepeatedAtLeast3Times = string.firstRepeatedChar(minRepetitions = 3)
+
+        Then("the result should be the character 'e'") {
+          firstCharRepeatedAtLeast3Times shouldBe 'e'
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          "3aeeeee1367614f3061d165a5fe3cac3".containsRepeatedChar('e', 5) shouldBe true
+    Given("the second sample string from the puzzle statement") {
+      val string = "3aeeeee1367614f3061d165a5fe3cac3"
+
+      When("I ask for the first character repeated at least 5 times") {
+        val firstCharRepeatedAtLeast5Times = string.firstRepeatedChar(minRepetitions = 5)
+
+        Then("the result should be the character 'e'") {
+          firstCharRepeatedAtLeast5Times shouldBe 'e'
         }
       }
     }
