048fa5ce70bb6ef31dcd2d94cf9c3604d8f48e3b
Stuart Kent <skent.dev@gmail.com>
2016-12-26 16:50:29 -0600

    Clean up day 7 tests

--- day7/src/test/kotlin/IPAddressCandidateTests.kt
+++ day7/src/test/kotlin/IPAddressCandidateTests.kt
@@ -3,66 +3,98 @@ import io.kotlintest.specs.BehaviorSpec
 class IPAddressCandidateTests : BehaviorSpec() {
 
   init {
-    Given("") {
-      When("") {
-        Then("") {
-          IPAddressCandidate("abba[mnop]qrst").supportsTransportLayerSnooping shouldBe true
+    Given("an IP address candidate with an ABBA sequence outside brackets and no ABBA sequence inside brackets") {
+      val ipAddressCandidate = IPAddressCandidate("abba[mnop]qrst")
+
+      When("I ask if this IP address candidate supports TLS") {
+        val ipAddressSupportsTLS = ipAddressCandidate.supportsTransportLayerSnooping
+
+        Then("the answer should be true") {
+          ipAddressSupportsTLS shouldBe true
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          IPAddressCandidate("abcd[bddb]xyyx").supportsTransportLayerSnooping shouldBe false
+    Given("an IP address candidate with an ABBA sequence outside brackets and an ABBA sequence inside brackets") {
+      val ipAddressCandidate = IPAddressCandidate("abcd[bddb]xyyx")
+
+      When("I ask if this IP address candidate supports TLS") {
+        val ipAddressSupportsTLS = ipAddressCandidate.supportsTransportLayerSnooping
+
+        Then("the answer should be false") {
+          ipAddressSupportsTLS shouldBe false
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          IPAddressCandidate("aaaa[qwer]tyui").supportsTransportLayerSnooping shouldBe false
+    Given("an IP address candidate with an invalid AAAA sequence outside brackets") {
+      val ipAddressCandidate = IPAddressCandidate("aaaa[qwer]tyui")
+
+      When("I ask if this IP address candidate supports TLS") {
+        val ipAddressSupportsTLS = ipAddressCandidate.supportsTransportLayerSnooping
+
+        Then("the answer should be false") {
+          ipAddressSupportsTLS shouldBe false
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          IPAddressCandidate("ioxxoj[asdfgh]zxcvbn").supportsTransportLayerSnooping shouldBe true
+    Given("an IP address candidate with a nested ABBA sequence outside brackets and no ABBA sequence inside brackets") {
+      val ipAddressCandidate = IPAddressCandidate("ioxxoj[asdfgh]zxcvbn")
+
+      When("I ask if this IP address candidate supports TLS") {
+        val ipAddressSupportsTLS = ipAddressCandidate.supportsTransportLayerSnooping
+
+        Then("the answer should be true") {
+          ipAddressSupportsTLS shouldBe true
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          IPAddressCandidate("aba[bab]xyz").supportsSuperSecretListening shouldBe true
+    Given("an IP address candidate with an ABA sequence outside brackets and the corresponding BAB sequence inside brackets") {
+      val ipAddressCandidate = IPAddressCandidate("aba[bab]xyz")
+
+      When("I ask if this IP address candidate supports SSL") {
+        val ipAddressSupportsSSL = ipAddressCandidate.supportsSuperSecretListening
+
+        Then("the answer should be true") {
+          ipAddressSupportsSSL shouldBe true
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          IPAddressCandidate("xyx[xyx]xyx").supportsSuperSecretListening shouldBe false
+    Given("an IP address candidate with an ABA sequence outside brackets and no corresponding BAB sequence inside brackets") {
+      val ipAddressCandidate = IPAddressCandidate("xyx[xyx]xyx")
+
+      When("I ask if this IP address candidate supports SSL") {
+        val ipAddressSupportsSSL = ipAddressCandidate.supportsSuperSecretListening
+
+        Then("the answer should be false") {
+          ipAddressSupportsSSL shouldBe false
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          IPAddressCandidate("aaa[kek]eke").supportsSuperSecretListening shouldBe true
+    Given("an IP address candidate with an invalid AAA sequence and a valid ABA sequence outside brackets and the corresponding BAB sequence inside brackets") {
+      val ipAddressCandidate = IPAddressCandidate("aaa[kek]eke")
+
+      When("I ask if this IP address candidate supports SSL") {
+        val ipAddressSupportsSSL = ipAddressCandidate.supportsSuperSecretListening
+
+        Then("the answer should be true") {
+          ipAddressSupportsSSL shouldBe true
         }
       }
     }
 
-    Given("") {
-      When("") {
-        Then("") {
-          IPAddressCandidate("zazbz[bzb]cdb").supportsSuperSecretListening shouldBe true
+    Given("an IP address candidate with multiple ABA sequences outside brackets and a corresponding BAB sequence inside brackets") {
+      val ipAddressCandidate = IPAddressCandidate("zazbz[bzb]cdb")
+
+      When("I ask if this IP address candidate supports SSL") {
+        val ipAddressSupportsSSL = ipAddressCandidate.supportsSuperSecretListening
+
+        Then("the answer should be true") {
+          ipAddressSupportsSSL shouldBe true
         }
       }
     }
