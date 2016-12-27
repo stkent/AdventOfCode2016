07542765c8af0427501375cc3245d6c818952c69
Stuart Kent <skent.dev@gmail.com>
2016-12-26 18:52:29 -0600

    Refactor day 3 tests

--- day3/src/test/kotlin/BaseTriangleCheckerTests.kt
+++ day3/src/test/kotlin/BaseTriangleCheckerTests.kt
@@ -4,29 +4,35 @@ class BaseTriangleCheckerTests : BehaviorSpec() {
 
   init {
     Given("a BaseTriangleChecker") {
-      When("I check all permutations of a list of valid triangle side lengths") {
-        Then("all lists are assessed to represent a valid triangle") {
-          val baseTriangleChecker = TestBaseTriangleChecker()
+      val baseTriangleChecker = TestBaseTriangleChecker()
 
-          baseTriangleChecker.isValidTriangle(listOf(3, 4, 5)) shouldBe true
-          baseTriangleChecker.isValidTriangle(listOf(3, 5, 4)) shouldBe true
-          baseTriangleChecker.isValidTriangle(listOf(4, 3, 5)) shouldBe true
-          baseTriangleChecker.isValidTriangle(listOf(4, 5, 3)) shouldBe true
-          baseTriangleChecker.isValidTriangle(listOf(5, 3, 4)) shouldBe true
-          baseTriangleChecker.isValidTriangle(listOf(5, 4, 3)) shouldBe true
+      When("I check all permutations of a list of valid triangle side lengths") {
+        val allPermutationsRepresentValidTriangle = listOf(
+            listOf(3, 4, 5),
+            listOf(3, 5, 4),
+            listOf(4, 3, 5),
+            listOf(4, 5, 3),
+            listOf(5, 3, 4),
+            listOf(5, 4, 3))
+                .all { baseTriangleChecker.isValidTriangle(it) }
+
+        Then("it is confirmed that all permutations represent a valid triangle") {
+          allPermutationsRepresentValidTriangle shouldBe true
         }
       }
 
       When("I check all permutations of a list of invalid triangle side lengths") {
-        Then("all lists are assessed to represent an invalid triangle") {
-          val baseTriangleChecker = TestBaseTriangleChecker()
+        val allPermutationsRepresentInvalidTriangle = listOf(
+            listOf( 3,  4, 10),
+            listOf( 3, 10,  4),
+            listOf( 4,  3, 10),
+            listOf( 4, 10,  3),
+            listOf(10,  3,  4),
+            listOf(10,  4,  3))
+                .all { !baseTriangleChecker.isValidTriangle(it) }
 
-          baseTriangleChecker.isValidTriangle(listOf(3, 4, 10)) shouldBe false
-          baseTriangleChecker.isValidTriangle(listOf(3, 10, 4)) shouldBe false
-          baseTriangleChecker.isValidTriangle(listOf(4, 3, 10)) shouldBe false
-          baseTriangleChecker.isValidTriangle(listOf(4, 10, 3)) shouldBe false
-          baseTriangleChecker.isValidTriangle(listOf(10, 3, 4)) shouldBe false
-          baseTriangleChecker.isValidTriangle(listOf(10, 4, 3)) shouldBe false
+        Then("it is confirmed that no permutation represents a valid triangle") {
+          allPermutationsRepresentInvalidTriangle shouldBe true
         }
       }
     }
