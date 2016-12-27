fe2f2111bd93f986146aeeed3198e8652cdb6b53
Stuart Kent <skent.dev@gmail.com>
2016-12-26 19:37:03 -0600

    Refactor day 15 tests

--- day15/src/test/kotlin/SculptureTests.kt
+++ day15/src/test/kotlin/SculptureTests.kt
@@ -4,20 +4,20 @@ class SculptureTests : BehaviorSpec() {
 
   init {
     Given("a sculpture with disks of sizes 5 and 2 at initial positions 4 and 1") {
+      val sculpture = Sculpture(listOf(
+          Pair(Disc(numberOfPositions = 5), 4),
+          Pair(Disc(numberOfPositions = 2), 1)
+      ))
+
       When("I compute how many time steps I must wait to be able to successfully drop a capsule") {
+        var numberOfRotationsPerformed = 0
+
+        while (!sculpture.canCapsuleBeDropped()) {
+          sculpture.performOneRotation()
+          numberOfRotationsPerformed += 1
+        }
+
         Then("I am correctly told I must wait 5 time steps") {
-          val sculpture = Sculpture(listOf(
-              Pair(Disc(numberOfPositions = 5), 4),
-              Pair(Disc(numberOfPositions = 2), 1)
-          ))
-
-          var numberOfRotationsPerformed = 0
-
-          while (!sculpture.canCapsuleBeDropped()) {
-            sculpture.performOneRotation()
-            numberOfRotationsPerformed += 1
-          }
-
           numberOfRotationsPerformed shouldBe 5
         }
       }
