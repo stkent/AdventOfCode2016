fb6e8d20fd0299cfcebea170d22fbe771f2f902a
Stuart Kent <skent.dev@gmail.com>
2016-12-26 22:21:02 -0600

    Refactor day 1 tests

--- day01/src/test/kotlin/NavigatorTests.kt
+++ day01/src/test/kotlin/NavigatorTests.kt
@@ -6,56 +6,76 @@ class NavigatorTests : BehaviorSpec() {
 
   init {
     Given("a Navigator") {
+      val navigator = Navigator()
 
-      When("I enter the instructions \"RIGHT, 2, LEFT, 3\"") {
+      When("I follow the instructions \"RIGHT, 2, LEFT, 3\"") {
+        val instructions = listOf(
+            Instruction(RIGHT, 2),
+            Instruction(LEFT, 3)
+        )
+
+        val distanceTraveled = navigator.computeDistanceToFinalLocation(instructions)
+
         Then("I should end up 5 blocks away from my starting point") {
-          val instructions = listOf(
-              Instruction(RIGHT, 2),
-              Instruction(LEFT,  3)
-          )
-
-          Navigator().computeDistanceToFinalLocation(instructions) shouldBe 5
+          distanceTraveled shouldBe 5
         }
       }
+    }
 
-      When("I enter the instructions \"RIGHT, 2, RIGHT, 2, RIGHT, 2\"") {
+    Given("a Navigator") {
+      val navigator = Navigator()
+
+      When("I follow the instructions \"RIGHT, 2, RIGHT, 2, RIGHT, 2\"") {
+        val instructions = listOf(
+            Instruction(RIGHT, 2),
+            Instruction(RIGHT, 2),
+            Instruction(RIGHT, 2)
+        )
+
+        val distanceTraveled = navigator.computeDistanceToFinalLocation(instructions)
+
         Then("I should end up 2 blocks away from my starting point") {
-          val instructions = listOf(
-              Instruction(RIGHT, 2),
-              Instruction(RIGHT, 2),
-              Instruction(RIGHT, 2)
-          )
-
-          Navigator().computeDistanceToFinalLocation(instructions) shouldBe 2
+          distanceTraveled shouldBe 2
         }
       }
+    }
 
-      When("I enter the instructions \"RIGHT, 5, LEFT, 5, RIGHT, 5, RIGHT, 3\"") {
+    Given("a Navigator") {
+      val navigator = Navigator()
+
+      When("I follow the instructions \"RIGHT, 5, LEFT, 5, RIGHT, 5, RIGHT, 3\"") {
+        val instructions = listOf(
+            Instruction(RIGHT, 5),
+            Instruction(LEFT, 5),
+            Instruction(RIGHT, 5),
+            Instruction(RIGHT, 3)
+        )
+
+        val distanceTraveled = navigator.computeDistanceToFinalLocation(instructions)
+
         Then("I should end up 12 blocks away from my starting point") {
-          val instructions = listOf(
-              Instruction(RIGHT, 5),
-              Instruction(LEFT,  5),
-              Instruction(RIGHT, 5),
-              Instruction(RIGHT, 3)
-          )
-
-          Navigator().computeDistanceToFinalLocation(instructions) shouldBe 12
+           distanceTraveled shouldBe 12
         }
       }
+    }
 
-      When("I enter the instructions \"RIGHT, 8, RIGHT, 4, RIGHT, 4, RIGHT, 8\"") {
+    Given("a Navigator") {
+      val navigator = Navigator()
+
+      When("I follow the instructions \"RIGHT, 8, RIGHT, 4, RIGHT, 4, RIGHT, 8\"") {
+        val instructions = listOf(
+            Instruction(RIGHT, 8),
+            Instruction(RIGHT, 4),
+            Instruction(RIGHT, 4),
+            Instruction(RIGHT, 8)
+        )
+
+        val distanceToFirstLocationVisitedTwice = navigator.computeDistanceToFirstLocationVisitedTwice(instructions)
+
         Then("the first location visited twice should be 4 blocks from my starting point") {
-          val instructions = listOf(
-              Instruction(RIGHT, 8),
-              Instruction(RIGHT, 4),
-              Instruction(RIGHT, 4),
-              Instruction(RIGHT, 8)
-          )
-
-          Navigator().computeDistanceToFirstLocationVisitedTwice(instructions) shouldBe 4
+          distanceToFirstLocationVisitedTwice shouldBe 4
         }
       }
-
     }
   }
 
