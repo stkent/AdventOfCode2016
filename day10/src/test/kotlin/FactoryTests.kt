d4c7e0ee176ee39767b894b1158f7d11d7394d10
Stuart Kent <skent.dev@gmail.com>
2016-12-26 19:34:23 -0600

    Refactor day 10 tests

--- day10/src/test/kotlin/FactoryTests.kt
+++ day10/src/test/kotlin/FactoryTests.kt
@@ -4,18 +4,19 @@ class FactoryTests : BehaviorSpec() {
 
   init {
     Given("a factory") {
-      When("I process a set of instructions") {
-        Then("all chips should end up in the correct output bins") {
-          val factory = Factory()
-          factory.processInstructions(listOf(
-              "value 5 goes to bot 2",
-              "bot 2 gives low to bot 1 and high to bot 0",
-              "value 3 goes to bot 1",
-              "bot 1 gives low to output 1 and high to bot 0",
-              "bot 0 gives low to output 2 and high to output 0",
-              "value 2 goes to bot 2"
-          ))
+      val factory = Factory()
 
+      When("I process a set of instructions") {
+        factory.processInstructions(listOf(
+            "value 5 goes to bot 2",
+            "bot 2 gives low to bot 1 and high to bot 0",
+            "value 3 goes to bot 1",
+            "bot 1 gives low to output 1 and high to bot 0",
+            "bot 0 gives low to output 2 and high to output 0",
+            "value 2 goes to bot 2"
+        ))
+
+        Then("all chips should end up in the correct output bins") {
           factory.outputs shouldBe mapOf(
               Pair(0, 5),
               Pair(1, 2),
