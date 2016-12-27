7dc7442e1cb09e17e7ff3fd0447909cbb49317be
Stuart Kent <skent.dev@gmail.com>
2016-12-26 18:41:51 -0600

    Refactor day 2 tests

--- day2/src/test/kotlin/DiamondPadPinCalculatorTests.kt
+++ day2/src/test/kotlin/DiamondPadPinCalculatorTests.kt
@@ -4,19 +4,23 @@ class DiamondPadPinCalculatorTests : BehaviorSpec() {
 
   init {
     Given("a PIN calculator for a diamond shaped pad") {
+      val calculator = DiamondPadPinCalculator()
+
       When("I enter the instructions:" +
           "ULL" +
           "RRDDD" +
           "LURDL" +
           "UUUUD") {
 
+        val calculatedPin = calculator.calculatePinFromInstructions(listOf(
+            "ULL",
+            "RRDDD",
+            "LURDL",
+            "UUUUD"
+        ))
+
         Then("the calculated PIN should be 5DB3") {
-          DiamondPadPinCalculator().calculatePinFromInstructions(listOf(
-              "ULL",
-              "RRDDD",
-              "LURDL",
-              "UUUUD"
-          )) shouldBe "5DB3"
+          calculatedPin shouldBe "5DB3"
         }
       }
     }
--- day2/src/test/kotlin/SquarePadPinCalculatorTests.kt
+++ day2/src/test/kotlin/SquarePadPinCalculatorTests.kt
@@ -4,19 +4,23 @@ class SquarePadPinCalculatorTests : BehaviorSpec() {
 
   init {
     Given("a PIN calculator for a square shaped pad") {
+      val calculator = SquarePadPinCalculator()
+
       When("I enter the instructions:" +
           "ULL" +
           "RRDDD" +
           "LURDL" +
           "UUUUD") {
 
+        val calculatedPin = calculator.calculatePinFromInstructions(listOf(
+            "ULL",
+            "RRDDD",
+            "LURDL",
+            "UUUUD"
+        ))
+
         Then("the calculated PIN should be 1985") {
-          SquarePadPinCalculator().calculatePinFromInstructions(listOf(
-              "ULL",
-              "RRDDD",
-              "LURDL",
-              "UUUUD"
-          )) shouldBe "1985"
+          calculatedPin shouldBe "1985"
         }
       }
     }
